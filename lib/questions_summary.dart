18e11a850d2d3801973eff444f8e0f878fb8a48d
Aleena Saji <aleenasaji6678@gmail.com>
2025-04-20 22:53:46 +0530

    result

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,25 +7,30 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            // 'as' helps in type casting.
-            Text(((data['question_index'] as int) + 1).toString()),
-            Expanded( // Guarantees Column below to take the full width in the Row above.
-              child: Column(
-                children: [
-                  Text(data['question'] as String),
-                  SizedBox(height: 5),
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
-                ],
-              ),
-            ),
-          ],
-        );
-      }).toList(), // Iterable object can be converted to List.
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView( // This particular column is scrollable.
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                // 'as' helps in type casting.
+                Text(((data['question_index'] as int) + 1).toString()),
+                Expanded( // Guarantees Column below to take the full width in the Row above.
+                  child: Column(
+                    children: [
+                      Text(data['question'] as String),
+                      SizedBox(height: 5),
+                      Text(data['user_answer'] as String),
+                      Text(data['correct_answer'] as String),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }).toList(), // Iterable object can be converted to List.
+        ),
+      ),
     );
   }
 }
